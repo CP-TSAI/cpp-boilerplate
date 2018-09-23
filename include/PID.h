d83dbd6f6b926beb58416f09fb404d13a7177dfb
Chin-Po <ChinPo.Tsai@magna.com>
2018-09-23 10:00:17 -0400

    PID.h interface with Doxygen comment

--- include/PID.h
+++ include/PID.h
@@ -1,14 +1,40 @@
+/**
+* @file PID.h
+*
+* @brief PID class interface
+*  Define the PID controller with kp/ki/kd gain
+*  Compute the velocity value according to "setpoint" and "current velocity" 
+* 
+* @author:
+* @copyright:
+*/
+
 #ifndef INCLUDE_PID_H_
 #define INCLUDE_PID_H_
 
+
+/**
+* @brief PID class interface
+* @param _Kp, _Ki, _Kd The gain values of PID controller
+* @param constructor, destructor Initialization and memory control
+* @param Compute() return the new velocity according to input param
+*/
 class PID {
- private:
-  double _Kp, _Ki, _Kd;
- public:
-  PID(const double &Kp, const double &Ki, const double &Kd);
-  ~PID();
-  double Compute(const double &Set_Point, const double &Current_Velocity);
-  
+private:
+	double _Kp;
+	double _Ki;
+	double _Kd;
+
+public:
+	/* constructor*/
+	PID(const double &Kp, const double &Ki, const double &Kd);
+	
+	/* destructor */
+	~PID();
+	
+	
+	double Compute(const double &Set_Point, const double &Current_Velocity);
+
 };
 
 #endif
