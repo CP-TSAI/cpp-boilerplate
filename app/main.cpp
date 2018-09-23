39be63753ce031f4ff1692adae513451baba60a9
Chin-Po <ChinPo.Tsai@magna.com>
2018-09-23 08:38:25 -0400

    CMakeList.txt edited: add .h file for compilation

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,18 @@
-#include <iostream>
-#include <lib.hpp>
 
-int main()
-{
-    dummy();
-    return 0;
+#include <iostream>
+#include <PID.h>
+
+
+int main() {
+  const double Kp = 1.1;
+  const double Ki = 0.5;
+  const double Kd = 0.05;
+  PID Controller = PID(Kp, Ki, Kd);
+
+  const double Set_Point = 200;
+  const double Current_Velocity = 20;
+  const double Control = Controller.Compute(Set_Point, Current_Velocity);
+  std::cout << "New Velocity: " << (Control + Current_Velocity) << std::endl;
+  std::cout << "Control: " << Control << std::endl;
+  return 0;
 }
