e22612061858cc61fa78f02f0f14ea9fc57ef83e
Chin-Po <aupt23@gmail.com>
2018-09-25 17:42:14 -0400

    Doxygen comment added

--- test/test.cpp
+++ test/test.cpp
@@ -1,15 +1,22 @@
+/**
+ * @file test.cpp
+ * @brief Unit Tests for PID Controller Implementation
+ * @author Chin-Po Tsai
+ * @copyright 2018, Chin-Po Tsai
+ */
+
 #include <gtest/gtest.h>
 #include "PID.h"
 
 TEST(Compute, should_pass) {
-	PID controller = PID(1, 0, 0);
-	EXPECT_EQ(3, controller.Compute(4,1));
+    PID controller = PID(1, 0, 0);
+    EXPECT_EQ(3, controller.Compute(4,1));
 }
 
-TEST(setConstants, should_pass){
-	PID controller;
-	controller.setConstants(1.1, 0.5, 0.05);
-	EXPECT_EQ(1.1, controller.getKp());
-	EXPECT_EQ(0.5, controller.getKi());
-	EXPECT_EQ(0.05, controller.getKd());
+TEST(setConstants, should_pass) {
+    PID controller;
+    controller.setConstants(1.1, 0.5, 0.05);
+    EXPECT_EQ(1.1, controller.getKp());
+    EXPECT_EQ(0.5, controller.getKi());
+    EXPECT_EQ(0.05, controller.getKd());
 }
